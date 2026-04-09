a5962b794f2fec778d7ec1cabd9c4a01999b0271
Sanjay Agamamidi <sanjay.agamamidi@gmail.com>
2026-04-09 18:13:59 +0530

    feat: implement agentic system prompt and async chat execution flow

--- index.js
+++ index.js
@@ -13,15 +13,41 @@ const getWeatherDetails = (city = '') => {
     return 'City not found';
 }
 
+const SYSTEM_PROMPT = `
+You are a helpful assistant that provides weather information for cities around the world.
+You have these states: start, plan, action, observe and output.
+Wait for user prompt and first plan using the available tools
+After that is done, take action with appropriate tools and wait for observation based on the action.
+Once the observation is received, return the response based on start prompt and observation
+
+Available Tools:
+- getWeatherDetails(city: string): string
+This function is used to get the weather details of a city.
+
+Example:
+Start
+{"type":"user", "user": "What is the sum of weather of hyderabad and delhi?"},
+{"type":"plan", "plan": "call getWeatherDetails for hyderabad"},
+{"type":"action", "function": "getWeatherDetails", "arguments": {"city": "hyderabad"}},
+{"type":"observe", "observation": "30°C"},
+{"type":"plan", "plan": "call getWeatherDetails for delhi"},
+{"type":"action", "function": "getWeatherDetails", "arguments": {"city": "delhi"}},
+{"type":"observe", "observation": "25°C"},
+{"type":"output", "output": "The sum is 55°C"}
+
+
+`
 
 const user = "Hey, what is the weather of Hyderabad, telangana?";
 
-// const model = genAI.getGenerativeModel({ model: "gemma-3-4b-it" });
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
+const model = genAI.getGenerativeModel({ model: "gemma-3-4b-it" });
+// const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
-model.generateContent(user)
-    .then((result) => {
-        const response = result.response;
-        console.log("Response text:", response.text());
-    })
-    .catch((error) => console.error(error));
+const chat = async () => {
+    const result = await model.generateContent(SYSTEM_PROMPT + user)
+    const response = result.response;
+    // console.log(response)
+    console.log(response.text());
+}
+
+chat();
